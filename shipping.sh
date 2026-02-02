cf1cae4341a0cbfab6bf1078cf8b051a7c6fe5c7
BhavithaL <lbhavitha342@gmail.com>
2026-02-02 23:21:14 +0530

    shipping

--- shipping.sh
+++ shipping.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
 USERID=$(id -u)
-
-LOGS_FOLDER="/var/log/configure-roboshop"
+LOGS_FOLDER="/var/log/shell-roboshop"
 LOGS_FILE="$LOGS_FOLDER/$0.log"
 R="\e[31m"
-G="\e[32m"          #-e must to enable colour code
+G="\e[32m"
 Y="\e[33m"
-N="\e[0m" #normal
+N="\e[0m"
 SCRIPT_DIR=$PWD
 MYSQL_HOST=mysql.exploreops.online
 
@@ -19,23 +18,24 @@ fi
 mkdir -p $LOGS_FOLDER
 
 VALIDATE(){
-if [ $1 -ne 0 ]; then 
-    echo -e "$2 is $R FAILURE $N" | tee -a $LOGS_FILE #tee -> prints log/output on screen and in log file, -a is append(without override)
-else 
-    echo -e "$2 is $G SUCCESS $N" | tee -a $LOGS_FILE
-fi
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOGS_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOGS_FILE
+    fi
 }
 
 dnf install maven -y &>>$LOGS_FILE
-VALIDATE $? "Installing maven"
+VALIDATE $? "Installing Maven"
 
 id roboshop &>>$LOGS_FILE
 if [ $? -ne 0 ]; then
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
-    VALIDATE $? "Creating System User"
+    VALIDATE $? "Creating system user"
 else
-    echo -e "Roboshop user already exist...$Y SKIPPING $N"
-fi 
+    echo -e "Roboshop user already exist ... $Y SKIPPING $N"
+fi
 
 mkdir -p /app 
 VALIDATE $? "Creating app directory"
@@ -46,39 +46,39 @@ VALIDATE $? "Downloading shipping code"
 cd /app
 VALIDATE $? "Moving to app directory"
 
-rm -rf /app/* 
+rm -rf /app/*
 VALIDATE $? "Removing existing code"
 
-unzip /tmp/shippng.zip &>>$LOGS_FILE
-VALIDATE $? "Unzip shipping code"
+unzip /tmp/shipping.zip &>>$LOGS_FILE
+VALIDATE $? "Uzip shipping code"
 
 cd /app 
 mvn clean package &>>$LOGS_FILE
 VALIDATE $? "Installing and Building shipping"
 
 mv target/shipping-1.0.jar shipping.jar 
-VALIDATE $? "Moving and renaming shipping"
+VALIDATE $? "Moving and Renaming shipping"
 
-cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service 
-VALIDATE $? "Creating systemctl service"
+cp $SCRIPT_DIR/shipping.service /etc/systemd/system/shipping.service
+VALIDATE $? "Created systemctl service"
 
 dnf install mysql -y  &>>$LOGS_FILE
-VALIDATE $? "Installing Mysql"
-
+VALIDATE $? "Installing MySQL"
 
 mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities'
 if [ $? -ne 0 ]; then
+
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOGS_FILE
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql &>>$LOGS_FILE
     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOGS_FILE
-    VALIDATE $? "Loaded data into MySql"
-else 
-    echo "Data is already loaded...$Y SKIPPING $N"
+    VALIDATE $? "Loaded data into MySQL"
+else
+    echo -e "data is already loaded ... $Y SKIPPING $N"
 fi
 
-systemctl enable shipping  &>>$LOGS_FILE
+systemctl enable shipping &>>$LOGS_FILE
 systemctl start shipping
-VALIDATE $? "Enabling and started"
+VALIDATE $? "Enabled and started shipping"
 
 
 
