e417ecfabe8b12a6856ebf157015fd8273ef07f3
BhavithaL <lbhavitha342@gmail.com>
2026-02-02 17:09:28 +0530

    user

--- redis.sh
+++ redis.sh
@@ -33,7 +33,7 @@ VALIDATE $? "Disabling redis"
 dnf module enable redis:7 -y &>>$LOGS_FILE
 VALIDATE $? "Enabling redis 7"
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOGS_FILE 
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Allowing remote connections"
 
 systemctl enable redis &>>$LOGS_FILE
